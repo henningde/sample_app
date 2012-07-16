8c1b5890479d83d9752b7de53b9b99e9a8f3dd83
Henning <ubuntu-zweitag@ubuntuzweitag-VirtualBox.(none)>
2012-07-16 11:44:26 +0200

    Finish pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Ruby on Rails Tutorial')
+    end
+  end
+
+describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
+
+  it "should have the right title" do
+  visit '/static_pages/home'
+  page.should have_selector('title',
+                    :text => "Home")
+end
+
+  it "should have the right title" do
+  visit '/static_pages/help'
+  page.should have_selector('title',
+                    :text => "Help")
+end
+
+  it "should have the right title" do
+  visit '/static_pages/about'
+  page.should have_selector('title',
+                    :text => "About")
+end
+
+end
