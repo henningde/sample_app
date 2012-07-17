9c1ebfc5f6bb06a390f250eb357ec9892785377f
Henning <ubuntu-zweitag@ubuntuzweitag-VirtualBox.(none)>
2012-07-17 15:24:56 +0200

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,10 @@ validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   
   default_scope order: 'microposts.created_at DESC'
+
+def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
+  end
 end
