1834a4b72c4dd10ac30ebd58bf094c89cf81f06d
Henning <ubuntu-zweitag@ubuntuzweitag-VirtualBox.(none)>
2012-07-17 11:19:25 +0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,12 +25,32 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
+
+      
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+describe "after saving the user" do
+        before { click_button submit }
+        
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        it { should have_link('Sign out') }
       end
 
       it "should create a user" do
